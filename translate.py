4f02e9c2d3c514ebd36d5d9ef976db7f4d57adda
mkanoria <mayankkanoria@gmail.com>
2018-12-06 19:49:29 -0500

    Added translate.py

---
+++ translate.py
@@ -0,0 +1,23 @@
+#Function to use input word and translate into chosen language
+
+#install gcp translate
+#get API key and set to path
+from google.cloud import translate
+translate_client = translate.Client()
+from language_codes import get_code
+
+#word is the text to be translated
+#langauage is the language to be translated in
+
+def translate_word(word, language):
+    language_code = get_code(language)
+    translation = translate_client.translate(word, target_language = language_code)
+    print(word + " translates to " + translation['translatedText'])
+    values = {'orig': word, 'text': translation['translatedText']} 
+    return values
+
+# def main():
+#     translate_word("Hello", 'fr')
+
+# if __name__ == '__main__':
+#   main()
