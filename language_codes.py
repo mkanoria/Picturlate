c8d983e9adef7c2a9c12db9b815c6263196d2591
mkanoria <mayankkanoria@gmail.com>
2018-12-06 19:47:30 -0500

    Added language codes

---
+++ language_codes.py
@@ -0,0 +1,4 @@
+language_codes = {"English": "en", "Spanish": "es", "French": "fr", "Italian": "it", "Greek": "el", "Hindi": "hi", "Korean": "ko", "Chinese": "cmn", "Japanese": "ja", "Russian": "ru-RU"}
+
+def get_code(language):
+    return language_codes[language]
